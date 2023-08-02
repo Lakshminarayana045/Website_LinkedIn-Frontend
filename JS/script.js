d4e038cf4c3a1eca1d81c8e6cebf81d3d93c5a56
LakshmiNarayana  Konanki <131501781+Lakshminarayana045@users.noreply.github.com>
2023-08-02 10:25:02 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+// Sort Post
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  //Preloading
+  let preloader = document.getElementById('loader');
+  let messageBlock = document.getElementById('message-block')
+
+  function loading(){
+    preloader.style.display = 'none';
+    messageBlock.style.display = 'flex';
+  }
